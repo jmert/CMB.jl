272ee58db4b00b845f46ef78e1a85f2bb707d556
Justin Willmert <justin@jdjlab.com>
2017-02-21 18:38:00 -0600

    Add HEALPix function pix2ringidx_ring

--- src/healpix.jl
+++ src/healpix.jl
@@ -1,7 +1,7 @@
 module Healpix
     export
         nside2npix, nside2nring,
-        pix2ring_ring, pix2z_ring
+        pix2ring_ring, pix2ringidx_ring, pix2z_ring
 
     @inline function nside2npix(nside::N) where N<:Integer
         @boundscheck (ispow2(nside) || throw(DomainError()))
@@ -37,6 +37,39 @@ module Healpix
         end
     end
     @inline pix2ring_ring(nside::I, p) where I<:Integer = _pix2ring_ring(Val{nside}, p)
+
+    @generated function _pix2ringidx_ring_north{nside}(::Type{Val{nside}}, p)
+        north_cap_done = 2nside^2 - 2nside
+        return quote
+            $(Expr(:meta, :inline))
+            r = _pix2ring_ring_north(Val{$nside}, p)
+            i = ifelse(p < $north_cap_done,
+                       p + 1 - 2r*(r-1),
+                       mod(p - $north_cap_done, $(4nside)) + 1
+                      )
+            return (i,r)
+        end
+    end
+    @generated function _pix2ringidx_ring{nside}(::Type{Val{nside}}, p)
+        npix = 12nside^2
+        north_eq_done  = 6nside^2 + 2nside
+        north_cap_done = 2nside^2 - 2nside
+        return quote
+            $(Expr(:meta, :inline))
+            isnorth = p < $north_eq_done
+            p = ifelse(isnorth, p, $(npix-1) - p)
+            (i,r) = _pix2ringidx_ring_north(Val{$nside}, p)
+            return ifelse(isnorth,
+                          i,
+                          ifelse(p < $north_cap_done,
+                                 4r - i + 1,
+                                 $(4nside + 1) - i
+                                )
+                         )
+        end
+    end
+    @inline pix2ringidx_ring(nside::I, p) where I<:Integer =
+        _pix2ringidx_ring(Val{nside}, p)
 
     @generated function _pix2z_ring_north{nside}(::Type{Val{nside}}, p)
         north_cap_done = 2nside^2 - 2nside
--- test/healpix.jl
+++ test/healpix.jl
@@ -4,19 +4,25 @@ module Healpix
 
     # Read off ring beginning/ending for Nside=4 map from Figure 5, Gorski
     # et al (2004).
-    p_start_nside4 = [0, 4,12,24,40,56,72, 88,104,120,136,152,168,180,188]
-    p_end_nside4   = [3,11,23,39,55,71,87,103,119,135,151,167,179,187,191]
+    p_start_nside4 = [0; 4;12;24;40;56;72; 88;104;120;136;152;168;180;188]
+    p_end_nside4   = [3;11;23;39;55;71;87;103;119;135;151;167;179;187;191]
 
     # Known ring start and ends for each ring
     @test all(pix2ring_ring.(4, p_start_nside4) .== 1:15)
     @test all(pix2ring_ring.(4, p_end_nside4)   .== 1:15)
 
-    # Make sure ring start and ends agree on their z value
+    # Start of each ring is always 1
+    @test all(pix2ringidx_ring.(4, p_start_nside4) .== 1)
+    # End of rings vary based on whether in a cap or the equatorial belt
+    @test all(pix2ringidx_ring.(4, p_end_nside4) .== [4;8;12;16ones(Int,9);12;8;4])
+
+    # Make sure pixels at starts and ends of rings agree on their z value
     @test all(pix2z_ring.(4,p_start_nside4) .== pix2z_ring.(4,p_end_nside4))
 
     # Type inference doesn't handle the nside -> Val{nside} transformation
     # without losing type stability, so these currently fail.
     @test_broken @inferred pix2ring_ring(4, 0)
+    @test_broken @inferred pix2ringidx(4, 0)
     @test_broken @inferred pix2z_ring(4, 0)
 end
 
