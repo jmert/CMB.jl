01d522fdb19ebdcf1e9d641987c69ee7db9413d6
Justin Willmert <justin@jdjlab.com>
2017-07-31 18:41:04 -0500

    Make some simplifications/generalizations to map definitions

--- src/mapdefn.jl
+++ src/mapdefn.jl
@@ -2,11 +2,11 @@ importall .Mapping
 
 export BicepMap
 
-struct BicepMap <: AbstractPixelizedMap
-    xr::Tuple{Float64,Float64}
-    yr::Tuple{Float64,Float64}
-    sq::Float64
-    ps::Float64
+struct BicepMap{T} <: AbstractPixelizedMap
+    xr::Tuple{T,T}
+    yr::Tuple{T,T}
+    sq::T
+    ps::T
 end
 
 function BicepMap()
@@ -27,28 +27,19 @@ _bicep_x_tic(m::BicepMap) = (sx = (m.xr[2]-m.xr[1]) / _bicep_nx(m);
 _bicep_y_tic(m::BicepMap) = (sy = (m.yr[2]-m.yr[1]) / _bicep_ny(m);
                              (m.yr[1]+sy/2):sy:(m.yr[2]-sy/2))
 
-ringtype(m::Type{BicepMap}) = Int
-pixeltype(m::Type{BicepMap}) = Tuple{Int,Int}
+pixeltype(::Type{BicepMap{T}}) where {T} = Tuple{Int,Int}
 
 numrings(m::BicepMap) = _bicep_ny(m)
 numpixels(m::BicepMap) = _bicep_ny(m) * _bicep_nx(m)
 ringlength(m::BicepMap, r) = _bicep_nx(m)
 
 let RI, PI, RPI
-    RI = RingIterator{BicepMap}
-    PI = PixelIterator{BicepMap}
-    RPI = RingPixelIterator{BicepMap,Int}
+    RI{T} = RingIterator{BicepMap{T}}
+    PI{T} = PixelIterator{BicepMap{T}}
+    RPI{T} = RingPixelIterator{BicepMap{T}}
 
-    Base.start(I::RI)  = 1
-    Base.start(I::PI)  = 1
-    Base.start(I::RPI) = 1
-
-    Base.next(I::RI, state)  = (state, state+1)
-    Base.next(I::PI, state)  = ( (x,y)=fldmod1(state, _bicep_ny(I.m)); ((y,x), state+1))
-    Base.next(I::RPI, state) = ( (I.r,state), state+1 )
-
-    Base.done(I::RI,  state) = state > numrings(I.m)
-    Base.done(I::PI,  state) = state > numpixels(I.m)
-    Base.done(I::RPI, state) = state > ringlength(I.m, I.r)
+    Base.next(I::PI{T}, state)  where{T} = (
+            (x,y)=fldmod1(state, _bicep_ny(I.m)); ((y,x), state+1) )
+    Base.next(I::RPI{T}, state) where{T} = ( (I.r,state), state+1 )
 end
 
--- src/mapping.jl
+++ src/mapping.jl
@@ -5,7 +5,7 @@ export
     RingPixelizedMap,
     AbstractRingIterator, AbstractPixelIterator, AbstractRingPixelIterator,
     RingIterator, PixelIterator,RingPixelIterator,
-    ringtype, pixeltype, numrings, numpixels, ringlength,
+    pixeltype, numrings, numpixels, ringlength,
     eachring, eachpixel, eachringpixel
 
 """
@@ -24,40 +24,60 @@ MappingIterator = Union{AbstractRingIterator,
                         AbstractRingPixelIterator}
 Base.iteratorsize(::MappingIterator) = Base.HasLength()
 
+"""
+    pixeltype(M)
+
+The data type of a pixel index for the map definition `M`.
+"""
+function pixeltype end
+pixeltype(m::AbstractPixelizedMap) = pixeltype(typeof(m))
+
 struct RingIterator{M<:AbstractPixelizedMap} <: AbstractRingIterator
     m::M
 end
 struct PixelIterator{M<:AbstractPixelizedMap} <: AbstractPixelIterator
     m::M
 end
-struct RingPixelIterator{M<:AbstractPixelizedMap,R} <: AbstractRingPixelIterator
+struct RingPixelIterator{M<:AbstractPixelizedMap} <: AbstractRingPixelIterator
     m::M
-    r::R
+    r::Int64
 end
 
 eachring(m::M) where {M<:AbstractPixelizedMap} = RingIterator{M}(m)
 eachpixel(m::M) where {M<:AbstractPixelizedMap} = PixelIterator{M}(m)
-eachringpixel(m::M, r::R) where {M<:AbstractPixelizedMap,R} = RingPixelIterator{M,R}(m, r)
+eachringpixel(m::M, r) where {M<:AbstractPixelizedMap} = RingPixelIterator{M}(m, r)
+
+Base.start(I::RingIterator{M}) where {M} = Int64(1)
+Base.start(I::PixelIterator{M}) where {M} = Int64(1)
+Base.start(I::RingPixelIterator{M}) where {M} = Int64(1)
+
+Base.next(I::RingIterator{M}, state) where {M} = (state, state+1)
+
+Base.done(I::RingIterator{M}, state) where {M} = state > numrings(I.m)
+Base.done(I::PixelIterator{M}, state) where {M} = state > numpixels(I.m)
+Base.done(I::RingPixelIterator{M}, state) where {M} = state > ringlength(I.m, I.r)
 
 Base.length(I::RingIterator{M}) where {M} = numrings(I.m)
 Base.length(I::PixelIterator{M}) where {M} = numpixels(I.m)
 Base.length(I::RingPixelIterator{M}) where {M} = ringlength(I.m, I.r)
 
-Base.eltype(::Type{RingIterator{M}}) where {M} = ringtype(M)
+Base.eltype(::Type{RingIterator{M}}) where {M} = Int64
 Base.eltype(::Type{PixelIterator{M}}) where {M} = pixeltype(M)
 Base.eltype(::Type{RingPixelIterator{M}}) where {M} = pixeltype(M)
 
-struct RingPixelizedMap{Tp,Tr} <: AbstractPixelizedMap
-    pixels::AbstractVector{Tp}
-    rings::AbstractVector{Tr}
-    ringptr::AbstractVector{Int64}
+struct RingPixelizedMap{T,Tp} <: AbstractPixelizedMap
+    nring::Int64
+    npix::Int64
+    pixels::Vector{Tp}
+    ringptr::Vector{Int64}
+    ringtheta::Vector{T}
+    pixphi::Vector{T}
 end
 
-ringtype(m::Type{RingPixelizedMap{Tp,Tr}}) where {Tp,Tr} = Tr
-pixeltype(m::Type{RingPixelizedMap{Tp,Tr}}) where {Tp,Tr} = Tp
+pixeltype(m::Type{RingPixelizedMap{T,Tp}}) where {T,Tp} = Tp
 
-numrings(m::RingPixelizedMap) = length(m.rings)
-numpixels(m::RingPixelizedMap) = length(m.pixels)
+numrings(m::RingPixelizedMap) = m.nring
+numpixels(m::RingPixelizedMap) = m.npix
 function ringlength(m::RingPixelizedMap, r::Int)
     n = numrings(m)
     r > n && throw(BoundsError())
@@ -66,48 +86,47 @@ function ringlength(m::RingPixelizedMap, r::Int)
 end
 
 let RI, PI, RPI
-    RI{Tp,Tr} = RingIterator{RingPixelizedMap{Tp,Tr}}
-    PI{Tp,Tr} = PixelIterator{RingPixelizedMap{Tp,Tr}}
-    RPI{Tp,Tr} = RingPixelIterator{RingPixelizedMap{Tp,Tr},Tr}
+    RI{T,Tp} = RingIterator{RingPixelizedMap{T,Tp}}
+    PI{T,Tp} = PixelIterator{RingPixelizedMap{T,Tp}}
+    RPI{T,Tp} = RingPixelIterator{RingPixelizedMap{T,Tp}}
 
-    Base.start(I::RI{Tp,Tr})  where {Tp,Tr} = Int64(1)
-    Base.start(I::PI{Tp,Tr})  where {Tp,Tr} = Int64(1)
-    Base.start(I::RPI{Tp,Tr}) where {Tp,Tr} = Int64(0)
-
-    Base.next(I::RI{Tp,Tr},  state) where {Tp,Tr} = (I.m.rings[state], state+1)
-    Base.next(I::PI{Tp,Tr},  state) where {Tp,Tr} = (I.m.pixels[state], state+1)
-    Base.next(I::RPI{Tp,Tr}, state) where {Tp,Tr} = (I.m.pixels[I.m.ringptr[I.r]+state], state+1)
-
-    Base.done(I::RI{Tp,Tr},  state) where {Tp,Tr} = state > numrings(I.m)
-    Base.done(I::PI{Tp,Tr},  state) where {Tp,Tr} = state > numpixels(I.m)
-    Base.done(I::RPI{Tp,Tr}, state) where {Tp,Tr} = state ≥ ringlength(I.m, I.r)
+    Base.next(I::PI{T,Tp},  state) where {T,Tp} = (I.m.pixels[state], state+1)
+    Base.next(I::RPI{T,Tp}, state) where {T,Tp} = (I.m.pixels[I.m.ringptr[I.r]+state-1], state+1)
 end
 
 function Base.convert(::Type{RingPixelizedMap}, m::RingPixelizedMap)
-    return RingPixelizedMap(copy(m.pixels), copy(m.rings), copy(m.ringptr))
+    return RingPixelizedMap(m.nring,
+                            m.npix,
+                            copy(m.pixels),
+                            copy(m.ringptr),
+                            copy(m.ringtheta),
+                            copy(m.pixphi))
 end
 
-function Base.convert(::Type{RingPixelizedMap}, m::M) where M<:AbstractPixelizedMap
+function Base.convert(::Type{RingPixelizedMap{T}}, m::M) where {T,M<:AbstractPixelizedMap}
     Tp = pixeltype(M)
-    Tr = ringtype(M)
     nring = numrings(m)
     npix = numpixels(m)
 
     pixels = Vector{Tp}(npix)
-    rings = Vector{Tr}(nring)
     ringptr = Vector{Int64}(nring)
+    ringtheta = Vector{T}(nring)
+    pixphi = Vector{T}(npix)
 
     pixoff = 1
     @inbounds for (i,r) in enumerate(eachring(m))
-        rings[i] = r
         for p in eachringpixel(m, r)
             pixels[pixoff] = p
+            # TODO:
+            # pixphi[pixoff] = ...
             pixoff += 1
         end
         ringptr[i] = i==1 ? 1 : ringptr[i-1] + ringlength(m, r)
+        # TODO:
+        # ringtheta[i] = ...
     end
 
-    return RingPixelizedMap{Tp,Tr}(pixels, rings, ringptr)
+    return RingPixelizedMap{T,Tp}(nring, npix, pixels, ringptr, ringtheta, pixphi)
 end
 
 end
