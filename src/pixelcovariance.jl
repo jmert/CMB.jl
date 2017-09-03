013bb401ed042c476e80403250b9106862c07386
Justin Willmert <justin@jdjlab.com>
2017-09-03 14:37:19 -0500

    Eliminate 3 allocations in PixelCovarianceF!

--- src/pixelcovariance.jl
+++ src/pixelcovariance.jl
@@ -65,38 +65,35 @@ function PixelCovarianceF!(C::PixelCovarianceCoeff{T}, F::AbstractMatrix{T},
 
     @inbounds begin
         P = view(F, :, 1)
-        F10 = view(F, :, 2)
-        F12 = view(F, :, 3)
-        F22 = view(F, :, 4)
 
         # Fill with the P^2_ℓ(x) terms initially
         LegendreP!(C.λ, P, lmax, 2, x)
 
         # Clear out the ℓ == 0 and ℓ == 1 terms since they aren't defined, and then
         # compute the rest of the terms
-        F12[1] = zero(T)
-        F12[2] = zero(T)
-        F22[1] = zero(T)
-        F22[2] = zero(T)
+        F[1,3] = zero(T)
+        F[2,3] = zero(T)
+        F[1,4] = zero(T)
+        F[2,4] = zero(T)
         for ll=2:lmax
             lT = convert(T, ll)
             lp2 = lT + convert(T, 2)
             lm1 = lT - one(T)
             lm4 = lT - convert(T, 4)
 
-            F12[ll+1] =  C.β[ll+1] * (lp2*xy*P[ll] - (lm4*y + half*lT*lm1)*P[ll+1])
-            F22[ll+1] = 2C.β[ll+1] * (lp2*y*P[ll]  - lm1*xy*P[ll+1])
+            F[ll+1,3] =  C.β[ll+1] * (lp2*xy*P[ll] - (lm4*y + half*lT*lm1)*P[ll+1])
+            F[ll+1,4] = 2C.β[ll+1] * (lp2*y*P[ll]  - lm1*xy*P[ll+1])
         end
 
         # Now refill P with the P^0_ℓ(x) terms
         LegendreP!(C.λ, P, lmax, 0, x)
 
         # Compute the F10 terms with the P as is
-        F10[1] = zero(T)
-        F10[2] = zero(T)
+        F[1,2] = zero(T)
+        F[2,2] = zero(T)
         for ll=2:lmax
             lm1 = convert(T, ll) - one(T)
-            F10[ll+1] = C.α[ll+1] * (xy*P[ll] - (y + half*lm1)*P[ll+1])
+            F[ll+1,2] = C.α[ll+1] * (xy*P[ll] - (y + half*lm1)*P[ll+1])
         end
 
         # Now finally apply the normalization to the P^0_ℓ(x) function
