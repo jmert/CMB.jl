1f31486460d19c61117428e946cc7242f7328eb4
Justin Willmert <justin@jdjlab.com>
2017-09-26 20:27:22 -0500

    Simplify HEALPix implementation
    - Remove `@healpixkernel` macro
      * Overloading `broadcast` meant broadcast-fusioning didn't work as
        expected
      * Drops dependency on `MacroTools.jl`
    
    - Add type promotion and fix some type stability in `pix2...` functions
    
    - Add `pix2ang` and `pix2vec` functions
      * Adds dependency on `StaticArrays.jl`

--- src/healpix.jl
+++ src/healpix.jl
@@ -9,16 +9,15 @@ arXiv: astro-ph/0409513
 module Healpix
 
 export
-    npix2nside, nside2npix, nside2nring, nside2npixcap, nside2npixequ, nside2npixarea,
-    isnorth, issouth, isnorthcap, issouthcap, iscap,
-    isequbelt, isnorthequbelt, issouthequbelt,
-    pix2ring, pix2ringidx, pix2z, pix2theta, pix2phi,
+    npix2nside, nring2nside,
+    nside2npix, nside2nring, nside2npixcap, nside2npixequ, nside2npixarea,
+    isnorth, issouth,
+    isnorthcap, issouthcap, iscap,
+    isnorthequbelt, issouthequbelt, isequbelt,
+    pix2ring, pix2ringidx, pix2z, pix2theta, pix2phi, pix2ang, pix2vec,
     UNSEEN
 
-using MacroTools
-using MacroTools: flatten, match, ismatch, postwalk, striplines
-
-import Base: broadcast, broadcast!
+using StaticArrays
 
 """
     const UNSEEN = -1.6375e+30
@@ -28,130 +27,25 @@ pixel.
 """
 const UNSEEN = -1.6375e+30
 
-"""
-    macro healpixkernel(funcexpr)
+Base.@pure npix2nside(npix::Integer)   = trunc(typeof(npix), sqrt(npix/12))
+Base.@pure nring2nside(nring::Integer) = (nring + one(nring)) ÷ 4
 
-Aids in developing optimized kernel functions for the basic HEALPix routines. The
-argument is expected to be a function expression of two arguments: the `nside` and a
-pixel to operate upon `pix`.
+Base.@pure nside2npix(nside::Integer)    = 12*nside*nside
+Base.@pure nside2nring(nside::Integer)   =  4*nside - one(nside)
+Base.@pure nside2npixcap(nside::Integer) =  2*nside*(nside - one(nside))
+Base.@pure nside2npixequ(nside::Integer) =  2*nside*(3*nside + one(nside))
+Base.@pure nside2pixarea(nside::Integer) = 4π / nside2npix(nside)
 
-```julia
-pixelfunc(nside, pix) = ...
-```
-
-From the definition, a kernel function is automatically extracted with some
-[single-argument] function calls involving `nside` lifted out of the kernel function and
-replaced with variables; the variables are arguments to the kernel function, and the
-outer function is responsible for computing the cached values.
-
-This is all done so that optimized broadcast functions can also be automatically
-generated where the cache variables are computed outside of the broadcast loop, and only
-the kernel function is invoked on each iteration.
-"""
-macro healpixkernel(funcexpr)
-    funcexpr = flatten(shortdef(striplines(funcexpr)))
-    @capture(funcexpr, fn_(nside_, pix_) = body_)
-
-    kfn = Symbol("_$(fn)_kernel")
-
-    # List of special functions we know we can trivially transform
-    kernmap = Symbol[
-            :nside2npix, :nside2nring, :nside2npixcap,
-            :nside2npixequ, :nside2pixarea
-        ]
-
-    # Mapping from the replacement expressions to the generated symbol
-    cachemap = Dict{Any,Symbol}()
-
-    # Identifies callable expressions where the function is in kernmap and the only
-    # argument is exactly nside.
-    isnside2fn(x) = begin
-            isexpr(x, :call) || return false
-            y = namify(x)
-            y in kernmap || return false
-            ismatch(:($y($nside)), x) || return false
-            return true
-        end
-
-    # Permit arbitrary caching by parsing a "macro" of the form
-    #   @cache(expr)
-    # which is then lifted out of the kernel function.
-    iscachemacro(x) = begin
-            isexpr(x, :macrocall) || return false
-            namify(x) == Symbol("@cache") || return false
-            return true
-        end
-
-    makecache(x) = begin
-            if isnside2fn(x)
-                ex = x
-            elseif iscachemacro(x)
-                m = MacroTools.match(:(@cache(args_)), x)
-                ex = m[:args]
-            else
-                # Break out early if we didn't encounter a cacheable state.
-                return x
-            end
-            # At this point, ex is the expression that we actually want to
-            # cache and use for later.
-            if ex in keys(cachemap)
-                sym = cachemap[x]
-            else
-                sym = gensym(namify(ex))
-                cachemap[ex] = sym
-            end
-            return sym
-        end
-
-    cacheargs = Symbol[]
-    body = MacroTools.postwalk(makecache, body)
-
-    setup = Expr[:($v = $ex) for (ex,v) in cachemap]
-    cacheargs = values(cachemap)
-
-    quote
-        @inline function $kfn($pix, $nside, $(cacheargs...))
-            @fastmath begin
-                $body
-            end
-        end
-
-        function $fn($nside, $pix)
-            $(setup...)
-            return $kfn($pix, $nside, $(cacheargs...))
-        end
-
-        function Base.broadcast(::typeof($fn), $nside::Integer, $pix)
-            $(setup...)
-            return $kfn.($pix, $nside, $(cacheargs...))
-        end
-
-        function Base.broadcast!(::typeof($fn), A::AbstractArray, $nside::Integer, $pix)
-            $(setup...)
-            return A .= $kfn.($pix, $nside, $(cacheargs...))
-        end
-
-        Base.@__doc__ $fn
-    end |> esc
-end
-
-Base.@pure npix2nside(npix)     = trunc(Int, sqrt(npix/12))
-Base.@pure nside2npix(nside)    = 12*nside*nside
-Base.@pure nside2nring(nside)   =  4*nside - 1
-Base.@pure nside2npixcap(nside) =  2*nside*(nside - 1)
-Base.@pure nside2npixequ(nside) =  2*nside*(3*nside + 1)
-Base.@pure nside2pixarea(nside) = 4π / nside2npix(nside)
+Base.@pure isnorth(nside, p) = p < nside2npixequ(nside)
+Base.@pure issouth(nside, p) = p ≥ nside2npixequ(nside)
 
 Base.@pure isnorthcap(nside, p) = p < nside2npixcap(nside)
-Base.@pure issouthcap(nside, p) = p > nside2npix(nside) - nside2npixcap(nside) - 1
-Base.@pure iscap(nside, p)      = isnorthcap(n, p) | issouthcap(n, p)
+Base.@pure issouthcap(nside, p) = p ≥ nside2npix(nside) - nside2npixcap(nside)
+Base.@pure iscap(nside, p)      = isnorthcap(nside, p) | issouthcap(nside, p)
 
-Base.@pure isnorthequbelt(nside, p) = (nside2npixcap(nside) ≤ p) & (p < nside2npixequ(nside))
-Base.@pure issouthequbelt(nside, p) = (nside2npixequ(nside) ≤ p) & (p ≤ nside - nside2npixcap(nside) - 1)
+Base.@pure isnorthequbelt(nside, p) = isnorth(nside, p) & ~isnorthcap(nside, p)
+Base.@pure issouthequbelt(nside, p) = issouth(nside, p) & ~issouthcap(nside, p)
 Base.@pure isequbelt(nside, p)      = ~iscap(n, p)
-
-Base.@pure isnorth(r, p) = p < nside2npixequ(r)
-Base.@pure issouth(r, p) = p ≥ nside2npixequ(r)
 
 # The function definitions are short enough that I wanted to keep them visually together
 # without having the documentation break them up. Add documentation now
@@ -163,6 +57,14 @@ Returns the equivalent `Nside` corresponding to the number of pixels `npix`. Not
 validation is performed, so non-conformant values of `npix` will give non-conformant
 Nside values.
 """ npix2nside(npix)
+
+"""
+    nring2nside(nring) -> Nside
+
+Returns the equivalent `Nside` corresponding to the number of iso-latitude rings `nring`.
+Note that no validation is performed, so non-conformant values of `nring` will give
+non-conformant Nside values.
+""" nring2nside(npix)
 
 """
     nside2npix(nside) -> N
@@ -263,16 +165,18 @@ equatorial ring) for an `nside` HEALPix map.
 Computes the ring index `i` for the given pixel `p`. `nside` is the Nside resolution
 factor.
 """
-@healpixkernel function pix2ring(nside, p)
-    p′ = p < nside2npixequ(nside) ? p : (nside2npix(nside)-1) - p
-    if p′ < nside2npixcap(nside)
-        i′ = trunc(Int, sqrt((p′+1)/2 - sqrt(convert(Float64,(p′+1)>>1)))) + 1
+@fastmath function pix2ring(nside::I, p::I) where I<:Integer
+    F = float(I)
+    p′ = isnorth(nside, p) ? p : (nside2npix(nside)-one(I)) - p
+    if isnorthcap(nside, p′)
+        i′ = trunc(I, sqrt((p′+one(I))/2 - sqrt(convert(F,(p′+one(I))>>1)))) + one(I)
     else
         i′ = div(p′-nside2npixcap(nside), 4nside) + nside
     end
-    i = p < nside2npixequ(nside) ? i′ : (nside2nring(nside)+1) - i′
+    i = isnorth(nside, p) ? i′ : (nside2nring(nside)+one(I)) - i′
     return i
 end
+pix2ring(nside, p) = pix2ring(promote(nside, p)...)
 
 """
     pix2ringidx(nside, p) -> j
@@ -280,19 +184,21 @@ end
 Computes the index `j` within the ring for the given pixel `p`. `nside` is the Nside
 resolution factor.
 """
-@healpixkernel function pix2ringidx(nside, p)
-    p′ = p < nside2npixequ(nside) ? p : (nside2npix(nside)-1) - p
-    if p′ < nside2npixcap(nside)
-        i′ = trunc(Int, sqrt((p′+1)/2 - sqrt(convert(Float64,(p′+1)>>1)))) + 1
-        j′ = p′ + 1 - nside2npixcap(i′)
+@fastmath function pix2ringidx(nside::I, p::I) where I<:Integer
+    F = float(I)
+    p′ = isnorth(nside, p) ? p : (nside2npix(nside)-1) - p
+    if isnorthcap(nside, p′)
+        i′ = trunc(I, sqrt((p′+one(I))/2 - sqrt(convert(F,(p′+one(I))>>1)))) + one(I)
+        j′ = p′ + one(I) - nside2npixcap(i′)
         l′ = 4i′
     else
-        j′ = rem(p′-nside2npixcap(nside), 4nside) + 1
+        j′ = rem(p′-nside2npixcap(nside), 4nside) + one(I)
         l′ = 4nside
     end
-    j = p < nside2npixequ(nside) ? j′ : (l′ - j′ + 1)
+    j = isnorth(nside, p) ? j′ : (l′ - j′ + one(I))
     return j
 end
+pix2ringidx(nside, p) = pix2ringidx(promote(nside, p)...)
 
 """
     pix2z(nside, p) -> z
@@ -300,18 +206,20 @@ end
 Computes the cosine of the colatitude `z` for the given pixel `p`. `nside` is the Nside
 resolution factor.
 """
-@healpixkernel function pix2z(nside, p)
-    p′ = p < nside2npixequ(nside) ? p : (nside2npix(nside)-1) - p
-    if p′ < nside2npixcap(nside)
-        i′ = trunc(Int, sqrt((p′+1)/2 - sqrt(convert(Float64,(p′+1)>>1)))) + 1
-        z′ = 1.0 - i′^2 / @cache(3nside^2)
+@fastmath function pix2z(nside::I, p::I) where I<:Integer
+    F = float(I)
+    p′ = isnorth(nside, p) ? p : (nside2npix(nside)-1) - p
+    if isnorthcap(nside, p′)
+        i′ = trunc(I, sqrt((p′+one(I))/2 - sqrt(convert(F,(p′+one(I))>>1)))) + one(I)
+        z′ = one(F) - i′^2 / (3nside^2)
     else
         i′ = div(p′-nside2npixcap(nside), 4nside) + nside
-        z′ = 4/3 - 2i′ / @cache(3nside)
+        z′ = 4/3 - 2i′ / (3nside)
     end
     z = p < nside2npixequ(nside) ? z′ : -z′
     return z
 end
+pix2z(nside, p) = pix2z(promote(nside, p)...)
 
 """
     pix2theta(nside, p) -> θ
@@ -319,18 +227,7 @@ end
 Computes the colatitude `θ` for the given pixel `p`. `nside` is the Nside resolution
 factor.
 """
-@healpixkernel function pix2theta(nside, p)
-    p′ = p < nside2npixequ(nside) ? p : (nside2npix(nside)-1) - p
-    if p′ < nside2npixcap(nside)
-        i′ = trunc(Int, sqrt((p′+1)/2 - sqrt(convert(Float64,(p′+1)>>1)))) + 1
-        z′ = 1.0 - i′^2 / @cache(3nside^2)
-    else
-        i′ = div(p′-nside2npixcap(nside), 4nside) + nside
-        z′ = 4/3 - 2i′ / @cache(3nside)
-    end
-    z = p < nside2npixequ(nside) ? z′ : -z′
-    return acos(z)
-end
+pix2theta(nside, p) = @fastmath acos(pix2z(promote(nside, p)...))
 
 """
     pix2phi(nside, p) -> ϕ
@@ -338,21 +235,52 @@ end
 Computes the azimuth `ϕ` for the given pixel `p`. `nside` is the Nside resolution
 factor.
 """
-@healpixkernel function pix2phi(nside, p)
-    p′ = p < nside2npix(nside)-nside2npixcap(nside) ? p : (nside2npix(nside)-1) - p
-    if p′ < nside2npixcap(nside)
-        i′ = trunc(Int, sqrt((p′+1)/2 - sqrt(convert(Float64,(p′+1)>>1)))) + 1
-        j′ = p′ + 1 - nside2npixcap(i′)
+@fastmath function pix2phi(nside::I, p::I) where I<:Integer
+    F = float(I)
+    p′ = isnorth(nside, p) ? p : (nside2npix(nside)-one(T)) - p
+    if isnorthcap(nside, p′)
+        i′ = trunc(I, sqrt((p′+one(I))/2 - sqrt(convert(F,(p′+one(I))>>1)))) + one(I)
+        j′ = p′ + one(I) - nside2npixcap(i′)
         ϕ′ = (π/2)/i′ * (j′ - 0.5)
     else
         i′,j′ = divrem(p′-nside2npixcap(nside), 4nside)
         i′ += nside
-        j′ += 1
-        ϕ′ = @cache(π/2/nside) * (j′ - 0.5*(1 + rem(i′-nside,2)))
+        j′ += one(I)
+        δ′ = rem(i′ - nside, 2)
+        ϕ′ = (π/2/nside) * (j′ - 0.5*(one(I) + δ′))
+        if issouth(nside, p) && δ′ ≠ 0
+            ϕ′ += π/2/nside
+        end
     end
-    ϕ = p < nside2npix(nside)-nside2npixcap(nside) ? ϕ′ : (2π - ϕ′)
+    ϕ = isnorth(nside, p) ? ϕ′ : (2π - ϕ′)
     return ϕ
 end
+pix2phi(nside, p) = pix2phi(promote(nside, p)...)
+
+"""
+    pix2ang(nside, p) -> (θ,ϕ)
+
+Computes the colatitude and azimuth pair `(θ,ϕ)` for the given pixel `p`.
+"""
+pix2ang(nside, p) = (pix2theta(nside, p), pix2phi(nside, p))
+
+"""
+    pix2vec(nside, p) -> SVector{3}(x, y, z)
+
+Computes the unit vector pointing to the pixel center of the given pixel `p`.
+"""
+@fastmath function pix2vec(nside::I, p::I) where I<:Integer
+    z = pix2z(nside, p)
+    ϕ = pix2phi(nside, p)
+    # If z ≈ ±1, this form should cause less "catastrophic cancellation" than the simpler
+    # invocation `one(z) - z*z` (I think...).
+    sinθ = sqrt((one(z)-z)*(one(z)+z))
+    # TODO: switch to sincos() when julia v0.7 is base version
+    x = sinθ * cos(ϕ)
+    y = sinθ * sin(ϕ)
+    return SVector{3}(x, y, z)
+end
+pix2vec(nside, p) = pix2vec(promote(nside, p)...)
 
 end # module Healpix
 
