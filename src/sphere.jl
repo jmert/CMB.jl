97095415ba537fa613bd324c0776a7d0bc0245f4
Justin Willmert <justin@jdjlab.com>
2017-09-30 16:33:06 -0500

    Add methods for `bearing`,`distance`,`cosdistance` which act on unit vectors
    Using `StaticArrays` allows these operations to be far faster for
    3-vectors than for angle pairs, especially since the 3-vectors can be
    cached if computations are done repeatedly.

--- src/sphere.jl
+++ src/sphere.jl
@@ -6,6 +6,9 @@ module Sphere
 export
     bearing, distance, cosdistance
 
+using StaticArrays
+import Base: @propagate_inbounds
+
 # Fast-path the isapprox tolerance for Float32 and Float64. The computation appears to be
 # too complex to constant-fold automatically, so we do it manually for these two most
 # important case.
@@ -19,14 +22,19 @@ rtepspi(::Type{T}) where {T<:AbstractFloat} = sqrt(eps(convert(T,π)))
 local ≈(x::T, y::T) where {T} = @fastmath x==y || abs(x-y) < rtepspi(T)
 
 """
-    bearing(θ₁, ϕ₁, θ₂, ϕ₂) -> α
-
-Calculates at the coordinate ``(θ_1, ϕ_1)`` the bearing angle ``α`` between North and the great
-circle connecting ``(θ_1, ϕ_1)`` to ``(θ_2,ϕ_2)``. Coordinates are to be given in radians where
-``θ`` is the colatitude angle from the North Pole and ``ϕ`` is the azimuthal angle.
+Calculates the bearing angle (``α``), defined as the angle between the meridian (at the
+first coordinate) and the great circle connection the first coordinate to the second. Angles
+are measured clockwise and will be in the range ``[0,π)``.
 """
 function bearing end
 
+"""
+    bearing(θ₁, ϕ₁, θ₂, ϕ₂) -> α
+
+Points on the sphere are given as coordinate pairs ``(θ₁,ϕ₁)`` and ``(θ₂,ϕ₂)`` where ``θ``
+is the colatitude angle from the North Pole and ``ϕ`` is the azimuthal angle, both in
+radians.
+"""
 bearing(θ₁, ϕ₁, θ₂, ϕ₂) = bearing(promote(θ₁, ϕ₁, θ₂, ϕ₂)...)
 
 function bearing(θ₁::T, ϕ₁::T, θ₂::T, ϕ₂::T) where T<:Number
@@ -50,18 +58,41 @@ function bearing(θ₁::T, ϕ₁::T, θ₂::T, ϕ₂::T) where T<:Number
     return @fastmath mod(α, π)
 end
 
+# Helper function to get the v × ẑ value, with a special case for SVectors so that the
+# compiler can inline all the operations without needing to actually allocate anything.
+cross_ẑ(v::AbstractVector) = [v[2], -v[1], zero(eltype(v))]
+cross_ẑ(v::SVector) = SVector(v[2], -v[1], zero(eltype(v)))
+
+"""
+    bearing(r₁, r₂) -> α
+
+Points on the sphere are given as unit vectors ``r₁`` and ``r₂``.
+"""
+@propagate_inbounds function bearing(r₁::AbstractVector, r₂::AbstractVector)
+    r₁₂ = r₁ × r₂
+    r₁′ = r₁ |> cross_ẑ
+    num = (r₁₂ × r₁′) ⋅ r₁
+    den = r₁₂ ⋅ r₁′
+    # Flip signs of both to move from quadrants 3 and 4 back into 1 and 2 iff the numerator
+    # is negative.
+    den = flipsign(den, num)
+    num = flipsign(num, num)
+    return @fastmath atan2(num, den)
+end
+
+"""
+Calculates the inner angle (``σ``) between unit vectors pointing from the center of the
+sphere to two points on its surface.
+"""
+function distance end
 
 """
     distance(θ₁, ϕ₁, θ₂, ϕ₂) -> σ
 
-Calculates the inner angle ``σ`` between unit vectors pointing from the center of the sphere
-to the two points ``(θ_1,ϕ_1)`` and ``(θ_2,ϕ_2)`` on its surface, where ``θ`` is the colatitude
-angle from the North Pole and ``ϕ`` is the azimuthal angle, both in radians.
-
-See also [`cosdistance`](@ref)
+Points on the sphere are given as coordinate pairs ``(θ₁,ϕ₁)`` and ``(θ₂,ϕ₂)`` where ``θ``
+is the colatitude angle from the North Pole and ``ϕ`` is the azimuthal angle, both in
+radians.
 """
-function distance end
-
 distance(θ₁, ϕ₁, θ₂, ϕ₂) = distance(promote(θ₁, ϕ₁, θ₂, ϕ₂)...)
 
 function distance(θ₁::T, ϕ₁::T, θ₂::T, ϕ₂::T) where T<:Number
@@ -86,16 +117,27 @@ function distance(θ₁::T, ϕ₁::T, θ₂::T, ϕ₂::T) where T<:Number
 end
 
 """
-    cosdistance(θ₁, ϕ₁, θ₂, ϕ₂) -> z
+    distance(r₁, r₂) -> σ
 
-Calculates the cosine of the inner angle ``z`` between unit vectors pointing from the center
-of the sphere to the two points ``(θ_1,ϕ_1)`` and ``(θ_2,ϕ_2)`` on its surface, where ``θ`` is the
-colatitude angle from the North Pole and ``ϕ`` is the azimuthal angle, both in radians.
+Points on the sphere are given as unit vectors ``r₁`` and ``r₂``.
+"""
+@propagate_inbounds function distance(r₁::AbstractVector, r₂::AbstractVector)
+    return acos(cosdistance(r₁, r₂))
+end
 
-See also [`distance`](@ref)
+"""
+Calculates the cosine of the inner angle (``z``) between unit vectors pointing from the
+center of the sphere to two points on its surface.
 """
 function cosdistance end
 
+"""
+    cosdistance(θ₁, ϕ₁, θ₂, ϕ₂) -> z
+
+Points on the sphere are given as coordinate pairs ``(θ₁,ϕ₁)`` and ``(θ₂,ϕ₂)`` where ``θ``
+is the colatitude angle from the North Pole and ``ϕ`` is the azimuthal angle, both in
+radians.
+"""
 cosdistance(θ₁, ϕ₁, θ₂, ϕ₂) = cosdistance(promote(θ₁, ϕ₁, θ₂, ϕ₂)...)
 
 function cosdistance(θ₁::T, ϕ₁::T, θ₂::T, ϕ₂::T) where T<:Number
@@ -118,5 +160,16 @@ function cosdistance(θ₁::T, ϕ₁::T, θ₂::T, ϕ₂::T) where T<:Number
 
     return @fastmath one(T) - 2*(sin(0.5*(θ₂-θ₁))^2 + sin(θ₁)*sin(θ₂)*sin(0.5*(ϕ₂-ϕ₁))^2)
 end
+
+"""
+    cosdistance(r₁, r₂) -> z
+
+Points on the sphere are given as unit vectors ``r₁`` and ``r₂``.
+"""
+@propagate_inbounds function cosdistance(r₁::AbstractVector, r₂::AbstractVector)
+    return r₁[1]*r₂[1] + r₁[2]*r₂[2] + r₁[3]*r₂[3]
+end
+
+
 
 end
