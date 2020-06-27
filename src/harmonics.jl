b492c66f51d63013f784ed8c029e5460e1572458
Justin Willmert <justin@willmert.me>
2020-06-27 15:25:08 -0500

    Add simple but slow reference implementation of harmonic synthesis

--- src/harmonics.jl
+++ src/harmonics.jl
@@ -13,17 +13,55 @@ function centered_range(start, stop, length)
     return range(start+Δ/2, stop-Δ/2, length=length)
 end
 
+# Brute-force synthesis of the real-space points at (θ, ϕ) given a set of alms.
+# The idea is that there are no optimizations, so it is much easier to verify the
+# correctness of the function at the expense of a huge hit to performance. This will
+# primarily only be useful for testing development of the more specialized algorithms.
+function synthesize_reference(alms::AbstractMatrix{T}, θ, ϕ) where {T<:Complex}
+    R = real(T)
+    axes(θ) == axes(ϕ) || throw(DimensionMismatch("θ and ϕ must have same axes"))
+    lmax, mmax = size(alms) .- 1
+
+    Λ = Matrix{R}(undef, size(alms)...)
+    Φ = Vector{T}(undef, mmax + 1)
+    syn = Array{R}(undef, size(θ)...)
+
+    @inbounds for I in eachindex(syn)
+        # λ_ℓ^m(cos θ) factors
+        λlm!(Λ, lmax, mmax, cos(θ[I]))
+        # e^{imϕ} factors
+        #   Using complex(cospi(), sinpi()) rather than exp(complex()) gives slightly more
+        #   accurate results for test healpix rings
+        @. Φ = complex(cospi((0:mmax) * ϕ[I]/π), sinpi((0:mmax) * ϕ[I]/π))
+
+        acc = zero(R)
+        # Σ_{ℓ = 0}^{ℓmax}
+        for ℓ in 0:lmax
+            # Σ_{m = 0}^{mmax}
+            acc += real(alms[ℓ+1,1] * Λ[ℓ+1,1])
+            for m in 1:min(ℓ, mmax)
+                # Assuming alms were sourced from real field, alms are constrained such
+                # that
+                #    a_{ℓ(-m)} Y_ℓ^(-m) + a_{ℓ(+m)} Y_ℓ^(+m)
+                #    == 2 * Re[ a_{ℓ(+m)} Y_ℓ^(+m) ]
+                acc += 2 * real(alms[ℓ+1,m+1] * Λ[ℓ+1,m+1] * Φ[m+1])
+            end
+        end
+        syn[I] = acc
+    end
+    return syn
+end
+
 # Simple reference function which synthesizes field for an equidistant-cylindrical
 # project (ECP) grid on the entire sphere.
 function synthesize_ecp(alms::Matrix{T}, nx::Integer, ny::Integer) where {T<:Complex}
-    lmax, mmax = last.(axes(alms)) .- 1
+    lmax, mmax = size(alms) .- 1
     nxr = nx ÷ 2 + 1 # real-symmetric FFT's Nyquist length (index)
 
     # pixel grid definition for ECP
     θ = centered_range(0.0, 1.0π, ny)
     ϕ = centered_range(0.0, 2.0π, nx)
-    Δθ, Δϕ = step(θ), step(ϕ)
-    θ₀, ϕ₀ = first(θ), first(ϕ)
+    ϕ₀ = first(ϕ)
 
     ecp = Matrix{real(T)}(undef, nx, ny) # transposed to have x-dim have stride 1
     Λ = zeros(real(T), lmax+1, mmax+1)
