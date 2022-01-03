50a4722b995013332631ad5f727e51777852f19d
Documenter.jl <documenter@juliadocs.github.io>
2022-01-03 00:08:47 +0000

    build based on 8a66b03

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -58,7 +58,7 @@ window.MathJax = {
 
 (function () {
     var script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.1.4/es5/tex-svg.js';
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.0/es5/tex-svg.js';
     script.async = true;
     document.head.appendChild(script);
 })();
