5d984e96349acbd47223623b47b169c02eeb9ece
Justin Willmert <justin@willmert.me>
2022-07-02 12:37:03 -0500

    New buffer manip utils: pack/unpack lower triangles to/from vectors
    These are useful in implementing linear algebra-like operations on
    spherical harmonic coefficients without unnecessarily including nearly
    half of the always-zero matrix elements above the main diagonal

--- src/numerics.jl
+++ src/numerics.jl
@@ -33,3 +33,212 @@ function quadprod(A::AbstractSparseMatrix, b::AbstractVecOrMat, n::Integer)
     end
     return C
 end
+
+"""
+    L = trillen(n::Int, m::Int = n)
+
+Calculates the length `L` of a vector required to store the elements of an `n` × `m`
+lower-triangular matrix.
+
+See also: [`trildim`](@ref)
+
+# Example
+```jldoctest; setup = (import CMB: trillen)
+julia> trillen(3)
+6
+
+julia> trillen(3, 3)
+6
+
+julia> trillen(3, 2)
+5
+```
+"""
+function trillen(n::Integer, m::Integer = n)
+    m <= n || throw(ArgumentError("require n ≤ m; got n = $n, m = $m"))
+    k = n - m
+    return n * (n + 1) ÷ 2 - k * (k + 1) ÷ 2
+end
+
+"""
+    n, m = trildim(L::Integer[, m::Integer])
+
+Calculates the dimensions `n` × `m` of a matrix capable of storing the `L` elements of a
+lower-triangular matrix. If `m < n`, then `m` must be provided.
+
+See also: [`trillen`](@ref)
+
+# Example
+```jldoctest; setup = (import CMB: trildim)
+julia> trildim(6)
+(3, 3)
+
+julia> trildim(6, 3)
+(3, 3)
+
+julia> trildim(5)
+ERROR: ArgumentError: length 5 incompatible with inferred triangle dims (2, 2)
+[...]
+
+julia> trildim(5, 2)
+(3, 2)
+
+julia> trildim(5, 1)
+(5, 1)
+```
+"""
+function trildim end
+
+function trildim(L::Integer)
+    n′ = (sqrt(8L + 1) - 1) / 2
+    n = trunc(Int, n′)
+    n == n′ || throw(ArgumentError("length $L incompatible with inferred triangle dims ($n, $n)"))
+    return (n, n)
+end
+function trildim(L::Integer, m::Integer)
+    n′ = (2L + m^2 - m) / 2m
+    n = trunc(Int, n′)
+    n == n′ || throw(ArgumentError("length $L incompatible with inferred triangle dims ($n, $m)"))
+    return (n, m)
+end
+
+"""
+    vec = packtril!(vec::AbstractVector, mat::AbstractMatrix)
+
+Packs the lower triangle of the matrix `mat` into the vector `vec`.
+
+See also: [`packtril`](@ref), [`trillen`](@ref), [`unpacktril!`](@ref)
+
+# Example
+```jldoctest; setup = (import CMB: packtril!)
+julia> packtril!(zeros(6), [1 0 0;
+                            2 3 0;
+                            4 5 6])
+6-element Vector{Float64}:
+ 1.0
+ 2.0
+ 4.0
+ 3.0
+ 5.0
+ 6.0
+
+julia> packtril!(zeros(5), [1 0;
+                            2 3;
+                            4 5])
+5-element Vector{Float64}:
+ 1.0
+ 2.0
+ 4.0
+ 3.0
+ 5.0
+```
+"""
+function packtril!(vec::AbstractVector, mat::AbstractMatrix)
+    L, (n, m) = length(vec), size(mat)
+    L == trillen(n, m) || throw(DimensionMismatch(
+            "cannot pack size ($n, $m) triangular matrix into length $L vector"))
+    i₀, j₀, k₀ = firstindex(mat, 1), firstindex(mat, 2), firstindex(vec, 1)
+    kk = 0
+    @inbounds for jj in 0:m-1, ii in jj:n-1
+        vec[k₀+kk] = mat[i₀+ii, j₀+jj]
+        kk += 1
+    end
+    return vec
+end
+
+"""
+    mat = unpacktril!(mat::AbstractMatrix, vec::AbstractVector)
+
+Unpacks the vector `vec` into the lower triangle of the matrix `mat`.
+
+See also: [`packtril!`](@ref), [`trildim`](@ref), [`unpacktril`](@ref)
+
+# Example
+```jldoctest; setup = (import CMB: unpacktril!)
+julia> unpacktril!(zeros(3, 3), 1:6)
+3×3 Matrix{Float64}:
+ 1.0  0.0  0.0
+ 2.0  4.0  0.0
+ 3.0  5.0  6.0
+
+julia> unpacktril!(zeros(3, 2), 1:5)
+3×2 Matrix{Float64}:
+ 1.0  0.0
+ 2.0  4.0
+ 3.0  5.0
+```
+"""
+function unpacktril!(mat::AbstractMatrix, vec::AbstractVector)
+    L, (n, m) = length(vec), size(mat)
+    L == trillen(n, m) || throw(DimensionMismatch(
+            "cannot unpack length $L vector into size ($n, $m) triangular matrix"))
+    i₀, j₀, k₀ = firstindex(mat, 1), firstindex(mat, 2), firstindex(vec, 1)
+    kk = 0
+    @inbounds for jj in 0:m-1, ii in jj:n-1
+        mat[i₀+ii, j₀+jj] = vec[k₀+kk]
+        kk += 1
+    end
+    return mat
+end
+
+"""
+    vec = packtril(mat::AbstractMatrix)
+
+Packs the lower triangle of matrix `mat` into the vector `vec`.
+
+See also: [`packtril!`](@ref), [`unpacktril`](@ref)
+
+# Example
+```jldoctest; setup = (import CMB: packtril)
+julia> packtril([1 0 0;
+                 2 3 0;
+                 4 5 6])
+6-element Vector{Int64}:
+ 1
+ 2
+ 4
+ 3
+ 5
+ 6
+```
+"""
+function packtril(mat::AbstractMatrix)
+    vec = fill!(similar(mat, trillen(size(mat)...)), zero(eltype(mat)))
+    return packtril!(vec, mat)
+end
+
+"""
+    mat = unpacktril(vec::AbstractVector[, m::Int])
+
+Unpacks the vector `vec` into the lower triangle of a matrix `mat`, inferring the dimensions
+based on the length `L = length(vec)` (and `m = size(mat, 2)` if necessary) with
+[`trildim`](@ref).
+
+See also: [`packtril`](@ref), [`unpacktril!`](@ref)
+
+# Example
+```jldoctest; setup = (import CMB: unpacktril)
+julia> unpacktril(1:6)
+3×3 Matrix{Int64}:
+ 1  0  0
+ 2  4  0
+ 3  5  6
+
+julia> unpacktril(1:5, 2)
+3×2 Matrix{Int64}:
+ 1  0
+ 2  4
+ 3  5
+```
+"""
+function unpacktril end
+
+function unpacktril(vec::AbstractVector)
+    mat = fill!(similar(vec, trildim(length(vec))...), zero(eltype(vec)))
+    return unpacktril!(mat, vec)
+end
+function unpacktril(vec::AbstractVector, m::Integer)
+    mat = fill!(similar(vec, trildim(length(vec), m)...), zero(eltype(vec)))
+    return unpacktril!(mat, vec)
+end
+
--- test/numerics.jl
+++ test/numerics.jl
@@ -1,4 +1,6 @@
-using Random, SparseArrays
+using Random
+using OffsetArrays
+using SparseArrays
 
 @testset "Unchecked square root ($T)" for T in [S for S in NumTypes if float(S) <: Base.IEEEFloat]
     using CMB: unchecked_sqrt
@@ -25,4 +27,88 @@ end
     B[:,i:i+j-1] .= b
     @test A * B * A' == quadprod(A, b, i)
     @test @inferred(quadprod(A, b, i)) isa SparseMatrixCSC{T, Int}
+end
+
+@testset "Triangular [Un]packing" begin
+    import CMB: trillen, trildim, packtril, unpacktril, packtril!, unpacktril!
+    import LinearAlgebra: LowerTriangular
+
+    test_sizes = ((i, j) for j in (0x1, 2, 5), i in (1, 2, 0x5) if j <= i)
+
+    @testset "Sizes & Lengths, (n, m) = ($n, $m)" for (n, m) in test_sizes
+        @test @inferred(trildim(trillen(n, m), m)) == (n, m)
+        n == m && @test @inferred(trildim(trillen(n, m))) == (n, m)
+
+        # only test error conditions when m != 1 since
+        #      L + 1 = trilen(n, 1) + 1
+        #   == L + 1 = trilen(n + 1, 1)
+        if m != 1
+            @test_throws ArgumentError trildim(trillen(n, m) + 1, m)
+            n == m && @test_throws ArgumentError trildim(trillen(n, m) + 1)
+        end
+    end
+
+    @testset "Pack ($n, $m)" for (n, m) in test_sizes
+        cols = repeat((1:n)', outer = (n, 1)) |> LowerTriangular |> x -> x[:, 1:m]
+        rows = repeat( 1:n,   outer = (1, n)) |> LowerTriangular |> x -> x[:, 1:m]
+        @test @inferred(packtril(cols)) == cols[cols .!= 0]
+        @test packtril(rows) == rows[rows .!= 0]
+    end
+
+    @testset "In-place pack ($n, $m)" for (n, m) in test_sizes
+        cols = repeat((1:n)', outer = (n, 1)) |> LowerTriangular |> x -> x[:, 1:m]
+        colsv = cols[cols .!= 0]
+        vec = Vector{Float64}(undef, trillen(n, m))
+        @test @inferred(packtril!(vec, cols)) == colsv
+        @test vec == colsv
+
+        # vector size must have correct length
+        @test_throws DimensionMismatch packtril!(vec[2:end], cols)
+        @test_throws DimensionMismatch packtril!([0; vec], cols)
+
+        # vector and matrix can have different axes, just need compatible lengths
+        vec′ = OffsetArray(vec, 0:length(vec)-1)
+        cols′ = OffsetArray(cols, 0:n-1, 1:m)
+        fill!(vec, 0.0)
+        @test packtril!(vec′, cols) !== nothing  # no error
+        @test vec == colsv
+        fill!(vec, 0.0)
+        @test packtril!(vec, cols′) !== nothing  # no error
+        @test vec == colsv
+    end
+
+    @testset "Unpack ($n, $m)" for (n, m) in test_sizes
+        cols = repeat((1:n)', outer = (n, 1)) |> LowerTriangular |> x -> x[:, 1:m]
+        rows = repeat( 1:n,   outer = (1, n)) |> LowerTriangular |> x -> x[:, 1:m]
+        colsv = cols[cols .!= 0]
+        rowsv = rows[rows .!= 0]
+        @test @inferred(unpacktril(colsv, m)) == cols
+        @test unpacktril(rowsv, m) == rows
+        if m == n
+            @test @inferred(unpacktril(colsv)) == cols
+            @test unpacktril(rowsv) == rows
+        end
+    end
+
+    @testset "In-place unpack ($n, $m)" for (n, m) in test_sizes
+        cols = repeat((1:n)', outer = (n, 1)) |> LowerTriangular |> x -> x[:, 1:m]
+        colsv = cols[cols .!= 0]
+        mat = zeros(n, m)  # need upper triangle to be zero initialized
+        @test @inferred(unpacktril!(mat, colsv)) == cols
+        @test mat == cols
+
+        # matrix size must have correct length
+        @test_throws DimensionMismatch unpacktril!(mat[2:end, 2:end], colsv)
+        @test_throws DimensionMismatch unpacktril!([zeros(1, m+1); zeros(n,1) mat], colsv)
+
+        # vector and matrix can have different axes, just need compatible lengths
+        colsv′ = OffsetArray(colsv, 0:length(colsv)-1)
+        mat′ = OffsetArray(mat, 0:n-1, 1:m)
+        fill!(mat, 0.0)
+        @test unpacktril!(mat′, colsv) !== nothing  # no error
+        @test mat == cols
+        fill!(mat, 0.0)
+        @test unpacktril!(mat, colsv′) !== nothing  # no error
+        @test mat == cols
+    end
 end
